e579014c5d32a7ae16f4e12f1f63f12de97c0619
VarHug <490641570@qq.com>
2020-07-14 09:58:25 +0800

    feat: 🎸 tokenpair page ui

--- src/pages/tokenpairDetail/index.js
+++ src/pages/tokenpairDetail/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import DexDesktopContainer from '_component/DexDesktopContainer';
+import DexDesktopInput from '_component/DexDesktopInput';
+import DexDesktopInputPair from '_component/DexDesktopInputPair';
+import DexTable from '_component/DexTable';
+import { toLocale } from '_src/locale/react-locale';
+import ont from '_src/utils/dataProxy';
+import URL from '_constants/URL';
+import { getDetailTokenPairCols } from '_src/utils/table';
 import './index.less';
 
 class TokenpairDetail extends Component {
@@ -7,20 +14,99 @@ class TokenpairDetail extends Component {
     super();
     this.state = {
       isActionLoading: false,
+      loading: false,
+      baseAsset: '',
+      quoteAsset: '',
+      operator: '',
+      tokenpairs: [],
+      pagination: {
+        page: 1,
+        total: 5,
+        per_page: 3,
+      }
     };
   }
 
+  componentDidMount() {
+    this.fetchTokenpairs();
+  }
+
+  onBaseAssetChange = (e) => {
+    this.setState({ baseAsset: e.target.value });
+  }
+
+  onQuoteAssetChange = (e) => {
+    this.setState({ quoteAsset: e.target.value });
+  }
+
+  onOperatorChange = (e) => {
+    this.setState({ operator: e.target.value });
+  }
+
+  onPageChange = (pageSize) => {
+    this.setState({
+      pagination: {
+        ...this.state.pagination,
+        page: pageSize
+      }
+    });
+  }
+
+  fetchTokenpairs = () => {
+    const { pagination } = this.state;
+    const params = {
+      page: pagination.page,
+      per_page: pagination.per_page,
+    };
+    this.setState({ loading: true });
+    ont.get(`${URL.GET_ACCOUNT_DEPOSIT}/${window.OK_GLOBAL.senderAddr}`, { params }).then(({ data }) => {
+      this.setState({ loading: false, tokenpairs: data });
+    }).catch(() => {
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
-    const { isActionLoading } = this.state;
+    const {
+      isActionLoading, loading,
+      baseAsset, quoteAsset, operator,
+      tokenpairs, pagination
+    } = this.state;
 
     return (
       <DexDesktopContainer
         className="tokenpair-detail-page"
         isShowAddress
+        isShowHelp
         needLogin
         loading={isActionLoading}
       >
-        123
+        <div className="tokenpair-detail-container">
+          <DexDesktopInput
+            label="DEX operator"
+            value={operator}
+            onChange={this.onOperatorChange}
+          />
+          <DexDesktopInputPair
+            label={toLocale('listToken.label')}
+            firstValue={baseAsset}
+            onFirstChange={this.onBaseAssetChange}
+            secondValue={quoteAsset}
+            onSecondChange={this.onQuoteAssetChange}
+          />
+          <button className="dex-desktop-btn" onClick={() => {}}>
+            Query
+          </button>
+          <DexTable
+            columns={getDetailTokenPairCols({ add: () => {}, withdraw: () => {} })}
+            dataSource={tokenpairs}
+            rowKey="product"
+            isLoading={loading}
+            empty={<div>{toLocale('tokenPair_emtpy')}</div>}
+            pagination={pagination}
+            onPageChange={this.onPageChange}
+          />
+        </div>
       </DexDesktopContainer>
     );
   }
--- src/utils/table.js
+++ src/utils/table.js
@@ -131,3 +131,57 @@ export const getDashboardTokenPairCols = ({ add, withdraw }) => {
     }
   ];
 };
+
+export const getDetailTokenPairCols = ({ add, withdraw }) => {
+  return [
+    {
+      title: toLocale('tokenPair_column_rank'),
+      key: 'rank',
+    },
+    {
+      title: 'DEX operator',
+      key: 'owner',
+    },
+    {
+      title: toLocale('tokenPair_column_tokenPair'),
+      key: 'product',
+      render: (text) => {
+        return util.getShortName(text);
+      }
+    },
+    {
+      title: toLocale('tokenPair_column_birth'),
+      key: 'block_height',
+    },
+    {
+      title: toLocale('tokenPair_column_deposit'),
+      key: 'deposits',
+      render: (text) => {
+        return calc.showFloorTruncation(text.amount, 8, false);
+      }
+    },
+    {
+      title: '',
+      key: 'add',
+      render: (text, { product }) => {
+        return (
+          <div className="complex-action-container">
+            <span
+              className="td-action"
+              onClick={add(product)}
+            >
+              {toLocale('tokenPair_cell_add')}
+            </span>
+            <div className="action-boundary" />
+            <span
+              className="td-action"
+              onClick={withdraw(product)}
+            >
+              {toLocale('tokenPair_cell_withdraw')}
+            </span>
+          </div>
+        );
+      }
+    }
+  ];
+};
