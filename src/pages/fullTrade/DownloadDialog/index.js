20e24debc8eccd2ea5922e20964441bd1ffaa28d
yuliang.zhang <yuliang.zhang@okcoin.net>
2020-09-16 19:36:53 +0800

    local node updating

--- src/pages/fullTrade/DownloadDialog/index.js
+++ src/pages/fullTrade/DownloadDialog/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Dialog } from '_component/Dialog';
 import { Circle } from 'rc-progress';
 import { calc } from '_component/okit';
+import Icon from '_component/IconLite';
 
 import './index.less';
 
@@ -19,19 +20,45 @@ const Process = ({ percent = 0, text }) => (
   </div>
 );
 
-const DownResult = ({ text }) => (
+const DownResult = ({ icon, text }) => (
   <div className="success">
     <div className="icon">
-
+      <Icon className={icon} isColor />
     </div>
     {text}
   </div>
 );
+const dialogConfigCommon = {
+  install: {
+    className: 'download-dialog',
+    cancelText: 'Cancel',
+    hideCloseBtn: false
+  },
+  downloading: {
+    className: 'status-dialog',
+    title: undefined,
+    cancelText: undefined,
+    confirmText: undefined,
+    hideCloseBtn: true
+  },
+  success: {
+    className: 'success-dialog',
+    title: undefined,
+    cancelText: undefined,
+    confirmText: 'GO Checked',
+  },
+  fail: {
+    className: 'fail-dialog',
+    title: undefined,
+    cancelText: 'Cancel',
+    confirmText: 'Try Again',
+  }
+};
 
 const dialogConfig = {
   install: {
     install: {
-      className: 'download-dialog',
+      ...dialogConfigCommon.install,
       title: 'Install Local node',
       children: (
         <div className="install">
@@ -39,31 +66,43 @@ const dialogConfig = {
           <p>Hardware requirement: CPU 4 core</p>
         </div>
       ),
-      cancelText: 'Cancel',
       confirmText: 'Install',
-      hideCloseBtn: false
     },
     downloading: {
-      className: 'status-dialog',
-      title: undefined,
-      children: <Process percent={0} text="installing..." />,
-      cancelText: undefined,
-      confirmText: undefined,
-      // hideCloseBtn: true
+      ...dialogConfigCommon.downloading,
+      children: <Process percent={0} text="Installing..." />
     },
     success: {
-      className: 'success-dialog',
-      title: undefined,
-      children: <DownResult text="Installed successfully！" />,
-      cancelText: undefined,
-      confirmText: 'GO Checked',
+      ...dialogConfigCommon.success,
+      children: <DownResult icon="icon-icon_success" text="Installed successfully" />
     },
     fail: {
-      className: 'fail-dialog',
-      title: undefined,
-      children: <DownResult text="Installed failed." />,
-      cancelText: 'Cancel',
-      confirmText: 'Try Again',
+      ...dialogConfigCommon.fail,
+      children: <DownResult icon="icon-icon_fail" text="Installed failed." />
+    }
+  },
+  update: {
+    install: {
+      ...dialogConfigCommon.install,
+      title: 'Local node upgrade notice',
+      children: (v) => (
+        <div className="install">
+          <p>You local node is outdated, please update to the latest version {v}</p>
+        </div>
+      ),
+      confirmText: 'Upgrade',
+    },
+    downloading: {
+      ...dialogConfigCommon.downloading,
+      children: <Process percent={0} text="Updating..." />
+    },
+    success: {
+      ...dialogConfigCommon.success,
+      children: <DownResult icon="icon-icon_success" text="Updated successfully！" />
+    },
+    fail: {
+      ...dialogConfigCommon.fail,
+      children: <DownResult icon="icon-icon_fail" text="Updated failed." />
     }
   }
 }
@@ -74,6 +113,7 @@ const onDownloadAndUpdate = () => {
   let option;
   let isFinish = false;
   let config = {};
+  let isInstall = true;
   const okchaindObj = store.get('okchaindObj');
   const cliObj = store.get('cliObj');
   const processTotal = okchaindObj.size + cliObj.size; // 2 assets will be download
@@ -90,11 +130,14 @@ const onDownloadAndUpdate = () => {
     }
   };
 
-  const setInstallDialog = () => {
+  const setInstallDialog = (type, version) => {
     option = config.install;
     option.onConfirm = () => {
       setDownloadingDialog('okchaind');
       emitter.emit('downloadOkchaind@download'); // trigger okchaind download
+    }
+    if (type === 'update') {
+      option.children = option.children(version);
     }
     updateDialog(option);
   }
@@ -107,7 +150,7 @@ const onDownloadAndUpdate = () => {
       const ratio = calc.div(okchaind + okchaincli, processTotal);
 
       const percent = calc.truncate(ratio * 100, 1);
-      option.children = <Process percent={percent} text="installing..." />
+      option.children = <Process percent={percent} text={isInstall ? 'Installing...' : 'Updating...'} />
       updateDialog(option);
     };
 
@@ -115,7 +158,6 @@ const onDownloadAndUpdate = () => {
   }
 
   const setSuceesDialog = () => {
-    console.log('setSuceesDialog');
     isFinish = true;
     option = config.success;
     option.onConfirm = () => dialog.destroy();
@@ -124,7 +166,6 @@ const onDownloadAndUpdate = () => {
 
   const setFailDialog = () => {
     isFinish = true;
-    console.log('setFailDialog')
     option = config.fail;
     option.onConfirm = () => {
       isFinish = false;
@@ -141,24 +182,24 @@ const onDownloadAndUpdate = () => {
   const initEmitter = () => {
     if (!isEmitterInit) {
       emitter.on('notDownload@Download', data => {
+        isInstall = true;
         config = dialogConfig.install;
         if(data.isRedownload) {
           setDownloadingDialog();
         } else {
-          setInstallDialog();
+          setInstallDialog('install');
         }
       });
 
       emitter.on('newVersionFound@download', data => {
+        isInstall = false;
         config = dialogConfig.update;
         if(data.isRedownload) {
           setDownloadingDialog();
         } else {
-          setInstallDialog();
+          setInstallDialog('update', data.tagName);
         }
       })
-
-      emitter.emit('windowReadyReceiveEvent');
 
       emitter.on('downloadProgress@okchaind', data => {
         setDownloadingDialog('okchaind', data.transferredBytes || 0);
@@ -181,6 +222,9 @@ const onDownloadAndUpdate = () => {
       emitter.on('downloadError', () => {
         setFailDialog();
       });
+
+      emitter.emit('windowReadyReceiveEvent'); // maybe github latest api so fast.
+
       isEmitterInit = true;
     }
   };
