b17f256fc5568acb30df9ee3a9a52ca93fc42d3b
yuliang.zhang <yuliang.zhang@okcoin.net>
2020-09-24 18:22:09 +0800

    fix logo name

--- src/desktop/pages/fullTrade/FullTradeHead.js
+++ src/desktop/pages/fullTrade/FullTradeHead.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import DesktopNodeMenu from '_app/component/DesktopNodeMenu';
 import DesktopLinkMenu from '_component/DesktopLinkMenu';
-import okexchainLogo from '_src/assets/images/OKExChain.png';
+import okexchainLogo from '_src/assets/images/OKExChainLogo.png';
 import * as CommonAction from '_src/redux/actions/CommonAction';
 import PageURL from '_src/constants/PageURL';
 import FullTradeTicker from '_src/pages/fullTrade/FullTradeTicker';
