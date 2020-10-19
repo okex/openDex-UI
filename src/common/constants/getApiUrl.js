abb13a842a9d63aa5df5f2714aa14a9b18c928e0
fafer <zhoucheng502@sina.com>
2020-10-19 19:52:58 +0800

    refactor: 节点请求地址

--- src/common/constants/getApiUrl.js
+++ src/common/constants/getApiUrl.js
@@ -1,4 +1,10 @@
-export default function (apiUrl = 'https://www.okex.com/') {
+import { storage } from '_component/okit';
+const currentNode = storage.get('currentNode');
+let apiUrl = 'https://www.okex.com';
+if(currentNode) apiUrl = currentNode.httpUrl || apiUrl;
+const APIURL = apiUrl;
+
+export default function (apiUrl = APIURL) {
   const protocol = window.location.protocol;
   if (/file/.test(protocol) || window.location.hostname === '127.0.0.1')
     return apiUrl;
