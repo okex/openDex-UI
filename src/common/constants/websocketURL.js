6ea82c23d9bf6089c2a40153aaecbe92e66f4459
fafer <zhoucheng502@sina.com>
2020-10-23 16:06:14 +0800

    fix: sVC 配置

--- src/common/constants/websocketURL.js
+++ src/common/constants/websocketURL.js
@@ -2,7 +2,7 @@ export default {
   DEX: {
     PROD: 'wss://dexcomreal.bafang.com:8443/ws/v3',
     QA: 'ws://real.okex.com:10442/ws/v3?_compress=false',
-    SVC: 'ws://okcoin-push-decentralization.dev-okex.svc.cluster.local:10442/ws/v3?_compress=false',
+    SVC: 'ws://okcoin-push-decentralization.test-e-okex.svc.test.local:10442/ws/v3?compress=true',
     DEV:
       'ws://okcoin-push-decentralization.dev-okex.svc.cluster.local:10442/ws/v3?_compress=false',
   },
