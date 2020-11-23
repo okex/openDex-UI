1de43a2907be07150347c7c6524f690c4f1060a1
lujie.xu@okcoin.net <lujie.xu@okcoin.net>
2020-11-23 16:41:40 +0800

    跟换dex-test页面的步骤一英文图片

--- src/common/locale/en.js
+++ src/common/locale/en.js
@@ -549,7 +549,7 @@ const enUS = {
   home_steps_item3_content:
     'For each transaction and transfer, you can query the corresponding information in the blockchain browser through the transaction hash value and address.',
   home_steps_item3_button: 'Check block data',
-  home_steps_item0_img: 'https://static.bafang.com/cdn/assets/imgs/209/8AF5AC5D994DF7A6.png',
+  home_steps_item0_img: 'https://static.bafang.com/cdn/assets/imgs/2011/F919A6FA182A5C38.png',
   home_steps_item1_img: 'https://static.bafang.com/cdn/assets/imgs/MjAyMDQ/9088F851DF59FD358F6EAA08BFDE08A4.png',
   home_steps_item2_img: 'https://static.bafang.com/cdn/assets/imgs/MjAyMDQ/1CDBD8D16A98147F82E9E7703FE9DE61.png',
   home_steps_item3_img: 'https://static.bafang.com/cdn/assets/imgs/209/2B5BC821D378FCE7.png',
