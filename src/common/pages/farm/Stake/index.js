25821e3bb7eb8c0c56961721d0f0ec11129b4bff
fafer <cheng.zhou@okg.com>
2021-01-07 14:35:48 +0800

    feat: farm

--- src/common/pages/farm/Stake/index.js
+++ src/common/pages/farm/Stake/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { toLocale } from '_src/locale/react-locale'
 import { getCoinIcon } from '../util/coinIcon';
+import classNames from 'classnames';
 import InputNum from '_component/InputNum';
 
 export default class Stake extends React.Component {
@@ -29,45 +30,50 @@ export default class Stake extends React.Component {
     return (
       <div className="stake-panel">
         <div className="stake-panel-title">{toLocale(locale)}<span className="close" onClick={onClose}>×</span></div>
-        <div className="space-between stake-panel-label">
-          <div className="left">{toLocale('Number')}</div>
-          <div className="right">{toLocale(avaliableLocale)}{data.avaliable}</div>
-        </div>
-        <div className="space-between stake-panel-input">
-          <div className="left">
-            <InputNum
-              type="text"
-              value={value}
-              onChange={this.onInputChange}
-              placeholder="0.00000000"
-              precision={8}
-            />
+        <div className="stake-panel-content">
+          <div className="space-between stake-panel-label">
+            <div className="left">{toLocale('Number')}</div>
+            <div className="right">{toLocale(avaliableLocale)}{data.avaliable}</div>
           </div>
-          <div className="right">
-            <div className="coin2coin">
-              <img src={getCoinIcon(data.lock_symbol)} />
-              <img src={getCoinIcon(data.yield_symbol)} />
-              <span>
-                {data.lock_symbol_dis}/{data.yield_symbol_dis}
-              </span>
+          <div className="stake-panel-input-wrap">
+            <div className="space-between stake-panel-input">
+              <div className="left">
+                <InputNum
+                  type="text"
+                  value={value}
+                  onChange={this.onInputChange}
+                  placeholder="0.00000000"
+                  precision={8}
+                />
+              </div>
+              <div className="right">
+                <div className="coin2coin">
+                  <img src={getCoinIcon(data.lock_symbol)} />
+                  <img src={getCoinIcon(data.yield_symbol)} />
+                  <span>
+                    {data.lock_symbol_dis}/{data.yield_symbol_dis}
+                  </span>
+                </div>
+              </div>
             </div>
+            {/* <div className="error-tip">*12121212</div> */}
           </div>
-        </div>
-        {isStake &&
-          <>
-            <div className="space-between stake-panel-detail">
-              <div className="left">{toLocale('Pool ratio')}</div>
-              <div className="right">{data.pool_ratio_dis}</div>
+          {isStake &&
+            <>
+              <div className="space-between stake-panel-detail">
+                <div className="left">{toLocale('Pool ratio')}</div>
+                <div className="right">{data.pool_ratio_dis}</div>
+              </div>
+              <div className="space-between stake-panel-detail">
+              <div className="left">{toLocale('FARM APY')}</div>
+              <div className="right">{data.total_apy} {data.farm_apy_dis}</div>
             </div>
-            <div className="space-between stake-panel-detail">
-            <div className="left">{toLocale('FARM APY')}</div>
-            <div className="right">{data.total_apy} {data.farm_apy_dis}</div>
-          </div>
-          </>
-        }
-        <div className="opts">
+            </>
+          }
+        </div>
+        <div className={classNames('stake-panel-footer',{nomargin:!isStake})}>
           <div className="btn cancel" onClick={onClose}>{toLocale('cancel')}</div>
-          <div className="btn" onClick={this.confirm}>{toLocale(locale)}</div>
+          <div className="btn disabled" onClick={this.confirm}>{toLocale(locale)}</div>
         </div>
       </div>
     );
