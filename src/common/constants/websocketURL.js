a1012b081b135a796c22af431f40d3c8590d786f
fafer <cheng.zhou@okg.com>
2021-01-11 19:30:17 +0800

    feat: wss线上环境推送地址区分国内、国外

--- src/common/constants/websocketURL.js
+++ src/common/constants/websocketURL.js
@@ -1,6 +1,9 @@
 export default {
   DEX: {
-    PROD: 'wss://dexcomreal.bafang.com:8443/ws/v3',
+    get PROD() {
+      if(window.okGlobal.ipRegion === 'CN') return 'wss://wsdex.coinall.ltd:8443/ws/v3';
+      return 'wss://wsdex.okex.com:8443/ws/v3';
+    } ,
     QA: 'ws://real.okex.com:10442/ws/v3?_compress=false',
     SVC:
       'ws://okcoin-push-decentralization.test-b-okex.svc.test.local:10442/ws/v3',
