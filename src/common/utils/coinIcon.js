ed4225aa83c6f2469a14858499e2c7d82d18c5da
fafer <cheng.zhou@okg.com>
2021-01-19 16:38:31 +0800

    feat: 新增ethk、ltck币种icon

--- src/common/utils/coinIcon.js
+++ src/common/utils/coinIcon.js
@@ -1,13 +1,15 @@
 import coinDefault from '_src/assets/images/icon_f2pool@2x.png';
 import coinOkt from '_src/assets/images/icon_usdt@2x.png';
 import defaultcoin from '_src/assets/images/defaultcoin.png';
+import coinEthk from '_src/assets/images/icon_ethk.png';
+import coinLtck from '_src/assets/images/icon_ltck.png';
 
 const icons = {
   OKT: coinOkt,
   BTCK: '//static.coinall.ltd/cdn/assets/imgs/2012/BE4B2476FC222ECB.png?x-oss-process=image/format,webp',
   USDT: '//static.coinall.ltd/cdn/assets/imgs/2011/6C04D5C74FCC0D3D.png',
-  ETHK: '',
-  LTCK: '',
+  ETHK: coinEthk,
+  LTCK: coinLtck,
   DOTK: '//static.coinall.ltd/cdn/assets/imgs/2012/1F10D45986407FF1.png',
   FILK: '//static.coinall.ltd/cdn/assets/imgs/2012/2895A447B916043F.png',
   USDC: '//static.coinall.ltd/cdn/assets/imgs/2011/635D4664F3F1C5E7.png',
