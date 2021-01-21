ed9f243ff28e838879187b9683a29a88f6f4a492
fafer <cheng.zhou@okg.com>
2021-01-21 14:52:15 +0800

    feat: icon

--- src/common/utils/coinIcon.js
+++ src/common/utils/coinIcon.js
@@ -6,15 +6,15 @@ import coinLtck from '_src/assets/images/icon_ltck.png';
 
 const icons = {
   OKT: coinOkt,
-  BTCK: '//static.coinall.ltd/cdn/assets/imgs/2012/BE4B2476FC222ECB.png?x-oss-process=image/format,webp',
-  USDT: '//static.coinall.ltd/cdn/assets/imgs/2011/6C04D5C74FCC0D3D.png',
-  ETHK: coinEthk,
-  LTCK: coinLtck,
-  DOTK: '//static.coinall.ltd/cdn/assets/imgs/2012/1F10D45986407FF1.png',
-  FILK: '//static.coinall.ltd/cdn/assets/imgs/2012/2895A447B916043F.png',
-  USDC: '//static.coinall.ltd/cdn/assets/imgs/2011/635D4664F3F1C5E7.png',
-  USDK: '//static.coinall.ltd/cdn/assets/imgs/2011/3405CB273D823CB6.png',
-  OKB: '//static.coinall.ltd/cdn/assets/imgs/2011/D1CF44F8F575D0C8.png'
+  'BTCK-BA9': '//static.coinall.ltd/cdn/assets/imgs/2012/BE4B2476FC222ECB.png?x-oss-process=image/format,webp',
+  'USDT-A2B': '//static.coinall.ltd/cdn/assets/imgs/2011/6C04D5C74FCC0D3D.png',
+  'ETHK-C63': coinEthk,
+  'LTCK-5CB': coinLtck,
+  'DOTK-4C0': '//static.coinall.ltd/cdn/assets/imgs/2012/1F10D45986407FF1.png',
+  'FILK-2EE': '//static.coinall.ltd/cdn/assets/imgs/2012/2895A447B916043F.png',
+  'USDC-E6C': '//static.coinall.ltd/cdn/assets/imgs/2011/635D4664F3F1C5E7.png',
+  'USDK-956': '//static.coinall.ltd/cdn/assets/imgs/2011/3405CB273D823CB6.png',
+  'OKB-C4D': '//static.coinall.ltd/cdn/assets/imgs/2011/D1CF44F8F575D0C8.png'
 };
 
 export function getCoinIcon(symbol) {
