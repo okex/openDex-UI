2b1f342c8751153c284776c5fed36b0b8a1275ad
fafer <cheng.zhou@okg.com>
2021-03-09 16:37:44 +0800

    feat: wallet connect

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -95,7 +95,6 @@ const base = {
       _component: path.resolve(__dirname, '../src/common/component/'),
       _constants: path.resolve(__dirname, '../src/common/constants/'),
       _app: path.resolve(__dirname, `../src/${client}/`),
-      '@okexchain/javascript-sdk':'@okexchain/javascript-sdk-qrcode',
     },
   },
 };
